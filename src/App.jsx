76a8de11c75ae53f41e15a49578ae1a427558ebc
Vasyl Ulianovskyi <vasia.kit434@gmail.com>
2024-07-25 00:50:26 +0300

    finish slider

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import Slider from "./Slider";
 
 const images = [
-  "https://images.pexels.com/photos/417074/pexels-photo-417074.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-  "https://assets.weforum.org/article/image/0ZUBmNNVLRCfn3NdU55nQ00UF64m2ObtcDS0grx02fA.jpg",
-  "https://onetreeplanted.org/cdn/shop/articles/nature_facts_1600x.jpg?v=1705008496",
-  "https://cdn.pixabay.com/photo/2018/02/02/17/29/nature-3125912_960_720.jpg",
-  "https://natureconservancy-h.assetsadobe.com/is/image/content/dam/tnc/nature/en/photos/Zugpsitze_mountain.jpg?crop=0%2C176%2C3008%2C1654&wid=4000&hei=2200&scl=0.752",
+  "https://images.wallpaperscraft.com/image/single/planets_galaxy_stars_146448_1920x1080.jpg",
+  "https://images.wallpaperscraft.com/image/single/galaxy_glow_spiral_142698_1920x1080.jpg",
+  "https://images.wallpaperscraft.com/image/single/cosmonaut_astronaut_art_134410_1920x1080.jpg",
+  "https://images.wallpaperscraft.com/image/single/exoplanet_atmosphere_clouds_stars_101205_1920x1080.jpg",
+  "https://images.wallpaperscraft.com/image/single/starry_sky_stars_night_117082_1920x1080.jpg",
+  "https://images.wallpaperscraft.com/image/single/pluto_planet_dwarf_planet_97479_1920x1080.jpg",
 ];
 
 function App() {
--- src/Slider/index.jsx
+++ src/Slider/index.jsx
@@ -1,44 +1,103 @@
-import { BsArrowsFullscreen } from "react-icons/bs";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { BsFullscreen, BsFullscreenExit } from "react-icons/bs";
+import { CiPlay1, CiPause1 } from "react-icons/ci";
+import { RxTrackPrevious, RxTrackNext } from "react-icons/rx";
 import styles from "./Slider.module.sass";
 
 function Slider({ images }) {
-  const [currentImgIndex, setCuttertImgIndex] = useState(0);
+  const [currentSlideIndex, setCurrentSlideIndex] = useState(0);
+  const [isPaused, setIsPaused] = useState(false);
+  const [delay, setDelay] = useState(5000);
+  const [isFullScreen, setIsFullScreen] = useState(false);
+  const [isControlPanelVisible, setIsControlPanelVisible] = useState(true);
 
   const nextSlide = () => {
-    setCuttertImgIndex((prevIndex) => (prevIndex + 1) % images.length);
+    setCurrentSlideIndex((prevIndex) => (prevIndex + 1) % images.length);
   };
 
   const prevSlide = () => {
-    setCuttertImgIndex(
+    setCurrentSlideIndex(
       (prevIndex) => (prevIndex - 1 + images.length) % images.length
     );
   };
 
+  useEffect(() => {
+    if (!isPaused) {
+      const interval = setInterval(() => {
+        setCurrentSlideIndex((prevIndex) => (prevIndex + 1) % images.length);
+      }, delay);
+      return () => clearInterval(interval);
+    }
+  }, [isPaused, delay, images.length]);
+
+  const handlePlayChange = () => {
+    setIsPaused((pause) => !pause);
+  };
+
+  const handleInterval = (e) => {
+    const value = Number(e.target.value);
+    if (isNaN(value) || value <= 0) {
+      setDelay(1 * 1000);
+    } else {
+      setDelay(value * 1000);
+    }
+  };
+
+  const Image = () =>
+    images.map((img, index) => (
+      <img
+        key={index}
+        src={img}
+        alt={`Slide ${index}`}
+        className={styles.slideImg}
+      />
+    ));
+
+  const handleFullScreenChange = () => {
+    if (!document.fullscreenElement) {
+      document.documentElement.requestFullscreen().catch((err) => {
+        console.error("Error attempting to enter full-screen mode:", err);
+      });
+    } else {
+      document.exitFullscreen().catch((err) => {
+        console.error("Error attempting to exit full-screen mode:", err);
+      });
+    }
+    setIsFullScreen((e) => !e);
+  };
+
   return (
-    <article className={styles.sliderElement}>
-      <div className={styles.slide}>
-        <img
-          className={styles.slideImg}
-          src={images[currentImgIndex]}
-          alt={`'Slide'${currentImgIndex}`}
-        />
+    <article
+      className={`${isFullScreen ? styles.fullscreen : styles.sliderElement}`}
+      onMouseEnter={() => setIsControlPanelVisible(true)}
+      onMouseLeave={() => setIsControlPanelVisible(false)}
+    >
+      <div
+        className={styles.slide}
+        style={{ transform: `translateX(-${currentSlideIndex * 100}%)` }}
+      >
+        <Image />
       </div>
-      <div className={styles.controlPanel}>
+      <div
+        className={styles.controlPanel}
+        style={{ display: isControlPanelVisible ? "flex" : "none" }}
+      >
         <button className={styles.controlBtn} onClick={prevSlide}>
-          {"<<"}
+          <RxTrackPrevious />
         </button>
-        <button className={styles.controlBtn}>Play</button>
+        <button className={styles.controlBtn} onClick={handlePlayChange}>
+          {isPaused ? <CiPlay1 /> : <CiPause1 />}
+        </button>
         <button className={styles.controlBtn} onClick={nextSlide}>
-          {">>"}
+          <RxTrackNext />
         </button>
-        <input
-          type="text"
-          // value={delay / 1000}
-          placeholder="Input time"
-        />
-        <button className={styles.fullScreenBtn}>
-          <BsArrowsFullscreen />
+
+        <input type="text" value={delay / 1000} onChange={handleInterval} />
+        <button
+          className={styles.fullScreenBtn}
+          onClick={handleFullScreenChange}
+        >
+          {isFullScreen ? <BsFullscreenExit /> : <BsFullscreen />}
         </button>
       </div>
     </article>
