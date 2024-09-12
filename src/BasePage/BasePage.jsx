5bec7ac3158374486c9e005f5212c369665b693e
Vasyl Ulianovskyi <vasia.kit434@gmail.com>
2024-09-12 23:10:24 +0300

    Add greating

--- src/BasePage/BasePage.jsx
+++ src/BasePage/BasePage.jsx
@@ -1,17 +1,18 @@
-import { Outlet } from "react-router-dom";
-import Header from "../Header/Header";
-import Footer from "../Footer/Footer";
+import { Outlet } from 'react-router-dom'
+import Header from '../Header/Header'
+import Footer from '../Footer/Footer'
 
-function BasePage() {
+function BasePage () {
   return (
     <>
       <Header />
-      <div className="mainContent">
+      <div className='mainContent'>
         <Outlet />
+        <h1>Welcome to my Router Site</h1>
       </div>
       <Footer />
     </>
-  );
+  )
 }
 
-export default BasePage;
+export default BasePage
