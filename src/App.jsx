e2d5fc72f6bc73311246401172aaf32e05de97af
Vasyl Ulianovskyi <vasia.kit434@gmail.com>
2024-09-12 23:05:41 +0300

    fix home page bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,24 @@
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
 
-import BasePage from "./BasePage/BasePage";
-import HomeEl from "./HomeEl/HomeEl";
-import ComponentsEl from "./ComponentsEl/ComponentsEl";
-import About from "./About/About";
-import NotFound from "./NotFound/NotFound";
+import BasePage from './BasePage/BasePage'
+import HomeEl from './HomeEl/HomeEl'
+import ComponentsEl from './ComponentsEl/ComponentsEl'
+import About from './About/About'
+import NotFound from './NotFound/NotFound'
 
-function App() {
+function App () {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<BasePage />}>
-          <Route index element={<HomeEl />} />
-          <Route path="/components/*" element={<ComponentsEl />} />
-          <Route path="/about" element={<About />} />
-          <Route path="*" element={<NotFound />} />
+        <Route path='/' element={<BasePage />}>
+          <Route path='/React-Router-App' element={<HomeEl />} />
+          <Route path='/components/*' element={<ComponentsEl />} />
+          <Route path='/about' element={<About />} />
+          <Route path='*' element={<NotFound />} />
         </Route>
       </Routes>
     </Router>
-  );
+  )
 }
 
-export default App;
+export default App
